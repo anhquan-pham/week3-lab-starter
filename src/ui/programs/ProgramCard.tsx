f843eb674200e3ca8d3100f9c754cd7b83081c7a
Santiago pabon <santiago.pabon72@gmail.com>
2026-01-26 12:36:38 -0700

    Finished todo for program card, program list, program section

--- src/ui/programs/ProgramCard.tsx
+++ src/ui/programs/ProgramCard.tsx
@@ -1,18 +1,25 @@
-import { StyleSheet } from "react-native";
+import { Pressable, StyleSheet, View, Text} from "react-native";
+import {Program} from "../../types/program";
 
 // TODO: Make a Props type for the props of our ProgramCard component
-
-export default function ProgramCard(/*TODO: Add props here (use desctructuring)*/) {
+type Props = {
+    program: Program;
+}
+export default function ProgramCard({program}: Props) {
   // TODO: Use descructuring to extract values from the program
   // If values need to be modified or altered for display (ex: a default value added if no delivery method is specified),
   // you can do that directly in the tsx code below, or create new variables here
   // if the values will be used in the log function as well, create variables for them here
+const{ id, name, credential, school, lengthInYears, numberOfCredits, deliveryMethod, careerPaths, note } = program;
+const ProgramSummary = `Credential: ${credential} | School: ${school} | Length: ${lengthInYears} years | Credits: ${numberOfCredits} | Delivery: ${deliveryMethod} | Carrer Path: ${careerPaths?.join(",")}`;
 
   // TODO: Create a function called logProgramInfo that logs a formatted summary of the program to the console
   // Example output:
   // Program: Diploma in Software Development | Credential: Diploma | School: School of Technology | Length: 2 years | Credits: 60 | Delivery: In-person
   // Career Paths: Software Developer, Web Developer, Mobile Application Developer
-
+ const logProgramInfo = () => {
+    console.log(ProgramSummary);
+ }
   // TODO: Create the TSX for the program card layout
   // There should be a Pressable as the root element, with onPress set to the logProgramInfo function created above
   // Inside the Pressable, create the following elements:
@@ -25,7 +32,18 @@ export default function ProgramCard(/*TODO: Add props here (use desctructuring)*
   // A View containing Text elements for each career path (if any exist) (use lines style for the view, and line style for each text)
   // If there is a note, a Text for the note
 
-  return null;
+  return <Pressable onPress={logProgramInfo} style={styles.card}>
+    <View style={styles.topRow}>
+      <Text style={styles.name}>{name}</Text>
+      <Text style={styles.badge}>{credential}</Text>
+    </View>
+    <Text style={styles.meta}>{school}</Text>
+    <Text style={styles.meta}>Length: {lengthInYears} years | Credits: {numberOfCredits} | Delivery: {deliveryMethod ?? "N/A"}</Text>
+    <View style={styles.lines}>
+      <Text style={styles.lines}>Career Path: {careerPaths?.join(", ")}</Text>
+    </View>
+    <Text style={styles.note}>{note}</Text>
+  </Pressable>
 }
 
 const styles = StyleSheet.create({
